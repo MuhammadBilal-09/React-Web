1802fd364100340ad03b01ee8d7fbb7060430918
Muhammad Bilal <117305519+MuhammadBilal-09@users.noreply.github.com>
2023-01-14 13:22:20 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function App() {
+  return (
+   <>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">🅱🅸🅻🅰🅻</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      
+    </ul>
+  </div>
+</nav>
+<br/>
+<div className='div'><p id='1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, laboriosam nulla voluptates eum vero magni qui dolores corporis praesentium earum sint quae esse neque ipsum, impedit numquam alias, accusamus ipsa quidem culpa saepe quos totam sit! Labore repellendus porro id aliquid quaerat? Sapiente at quidem ratione nostrum consequatur quo dicta omnis suscipit delectus saepe iusto assumenda quaerat unde possimus dignissimos cum esse deserunt distinctio, eligendi id aperiam,<a href=''>aut maxime.</a> Quidem sequi architecto aspernatur doloremque cum temporibus, itaque cupiditate modi expedita sit fugit eius ullam perferendis excepturi asperiores eaque maxime facilis, fugiat ea quisquam accusantium quod atque molestiae. Consequatur, voluptates ipsam?</p>
+<p id='2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit veritatis aliquid consequuntur, aspernatur id est vero rerum porro saepe maxime? Tempore molestiae voluptas nobis optio at. Libero eos corporis cum!</p>
+
+<img src="https://images.pexels.com/photos/35967/mini-cooper-auto-model-vehicle.jpg?cs=srgb&dl=pexels-pixabay-35967.jpg&fm=jpg"/>
+
+
+
+<br />
+<footer>
+  &#169;Copyright 
+</footer>
+
+
+</div>
+</>
+  )
+}
+
+export default App
